9243d978660061a4eb6d47c21793cc6b81a33a07
raphtalia <59540801+raphtalia@users.noreply.github.com>
2021-05-09 02:10:14 -0500

    Added Selene

--- src/main.lua
+++ src/main.lua
@@ -15,14 +15,14 @@ local fs = {}
 
 -- https://stackoverflow.com/questions/1426954/split-string-in-lua
 local function split(inputstr, sep)
-	if sep == nil then
-			sep = "%s"
-	end
-	local t={}
-	for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
-			table.insert(t, str)
-	end
-	return t
+    if sep == nil then
+        sep = "%s"
+    end
+    local t = {}
+    for str in string.gmatch(inputstr, "([^".. sep.. "]+)") do
+        table.insert(t, str)
+    end
+    return t
 end
 
 local function run(command)
